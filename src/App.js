d6cc151aaa06da06d3e2361d36b9651b69be03f4
unknown <logicndepth@gmail.com>
2021-06-19 23:51:30 -0700

    client side routing and pages are working, next adding state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import {Route, Link} from  'react-router-dom'
+import Form from './Form'
+
+
 
 const App = () => {
+
   return (
     <div>
-      <Route exact path = '/'/>
-      <h1>Lambda Eats</h1>
-      <nav>
-      <button>Order pizza</button> 
-      </nav>
-      <Link to = {'/pizza'}>order pizza </Link>
-      <p>You can remove this code and create your own header</p>
+      <header> 
+        <nav>
+          <h1>Lambda Eats</h1>
+          <Link to = '/'>Home</Link>
+        </nav>
+         <Route exact path = '/'/>
+       </header>
+       <Link to = '/pizza'>order pizza </Link>
+       <Route path = '/pizza' component={Form}/>
+      <p> You can remove this code and create your own header</p>
       
       
     </div>
